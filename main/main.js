d54b50a942edb39861a6aa3bb539c642ebbbfe05
gjx1 <gong0jiaxin@163.com>
2019-05-31 19:53:41 +0800

    add code to main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(s) {
+    var n_1 = ["._.","...","._.","._.","...","._.","._.","._.","._.","._."];
+    var n_2 = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
+    var n_3 = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
+    
+    var s_1 = "";
+    var s_2 = "";
+    var s_3 = "";
+
+    for(var i = s.length - 1; i>=0; --i){
+        var j = parseInt(s[i]);
+        if(i<s.length -1){
+            s_1 = " " + s_1;
+            s_2 = " " + s_2;
+            s_3 = " " + s_3;
+        }
+        s_1 = n_1[j] + s_1;
+        s_2 = n_2[j] + s_2;
+        s_3 = n_3[j] + s_3;
+    }
+
+    s_1 += "\n";
+    s_2 += "\n";
+    s_3 += "\n";
+    return s_1 + s_2 + s_3;
 };
